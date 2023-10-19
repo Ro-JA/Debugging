497f8b3091c8d90ad267eedb9dec71c31b9dbcfa
Roman <74274722+Ro-JA@users.noreply.github.com>
2023-10-19 14:33:29 +0900

    not work

--- app/src/main/java/com/example/debugging/MainActivity.kt
+++ app/src/main/java/com/example/debugging/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.debugging
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+}
+
+fun logging() {
+    Log.v(TAG, "Hello world!")
 }
