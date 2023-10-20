8ccc40de2b46c5555428c68e60b34cbfd990e80e
Roman <74274722+Ro-JA@users.noreply.github.com>
2023-10-20 13:15:09 +0900

    add error

--- app/src/main/java/com/example/debugging/MainActivity.kt
+++ app/src/main/java/com/example/debugging/MainActivity.kt
@@ -9,10 +9,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        logging()
+        division()
+
     }
 }
 
 fun logging() {
     Log.v(TAG, "Hello world!")
+}
+
+fun division() {
+    val numerator = 60
+    var denominator = 4
+    repeat(5) {
+        Log.v(TAG, "${numerator / denominator}")
+        denominator--
+    }
 }
